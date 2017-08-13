7dfeffc2d374c4453d6ad480524fa04c3ee56cf1
hiwin <mxdharni@gmail.com>
2017-08-13 17:07:26 +0800

    '第一次提交'

---
+++ 1.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+sum = 0
+
+# for x in [1,2,3,4,5,6,7,8,9]:
+# 	sum = sum+x
+# 	print sum
+
+#比如range(5)生成的序列是从0开始小于5的整数：
+# range(5)
+# print(range(6))
+
+# sum = 0
+# for x in range(101):
+# 	sum = sum+x
+# 	print(sum)
+
+# sum = 0
+# n = 99
+# while n>0:
+# 	sum += n
+# 	n -= 2
+# 	print sum
+
+# birth = input('birth: ')
+# birth = 111
+# if birth < 2000:
+# 	print '00qian'
+# else:
+# 	print '00hou'
+
+#dictionary类型
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+d['Adam'] = 67
+
+print 'hiwin' in d
+
+
+print abs(100)
+
+print cmp(1,2)
+
+print int(12.89)
+
