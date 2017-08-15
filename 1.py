21b4a3ab3e4376fcd2fbcf1bf0d8cb6f7982bf75
hiwin <mxdharni@gmail.com>
2017-08-15 23:43:32 +0800

    '第三次上传'

--- 1.py
+++ 1.py
@@ -113,3 +113,58 @@ print clac(1,2,3)
 print clac()
 nums = [3,4,5];
 print clac(*nums)
+
+def person(name ,age , **kw):
+    print 'name: ',name ,'age: ',age,'other: ',kw
+person('hiwin',18)
+person('Bob', 35, city='Beijing')
+kw = {'city': 'Beijing', 'job': 'Engineer'}
+person('hiwin' , 18 , **kw)
+
+def func(a, b, c=0, *args, **kw):
+    print 'a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw
+
+args = (3,4,5,6,8)
+kw = {'x':909}
+func(*args , **kw)
+
+def fact(n):
+    if n==1:
+        return 1
+    return n*fact(n-1)
+print fact(3)
+
+#使用递归函数需要注意防止栈溢出。在计算机中，函数调用是通过栈（stack）这种数据结构实现的，每当进入一个函数调用，栈就会加一层栈帧，每当函数返回，栈就会减一层栈帧。由于栈的大小不是无限的，所以，递归调用的次数过多，会导致栈溢出。可以试试fact(1000)：
+#尾递归函数，在函数返回的时候，调用自身本身，并且，return语句不能包含表达式。这样，编译器或者解释器就可以把尾递归做优化，使递归本身无论调用多少次，都只占用一个栈帧，不会出现栈溢出的情况。
+
+# def new_fact(n):
+#     return fact_iter(n,1)
+
+# def fact_iter(num , product):
+#     if num == 1:
+#         return product
+#     return fact_iter(num-1 , num*product)
+
+# print new_fact(3)
+
+# L = []
+# n = 1
+# while n<=99:
+#     L.append(n)
+#     n = n+2
+# print L
+
+#python 切片操作
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+
+print L[0:3]
+print L[:3]
+
+K = range(100)
+print K[-10:]
+print K[:10:2]
+print K[::10]
+print K[::]
+
+T = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+print T[:3]
